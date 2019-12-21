a60811a4722bc945dbb49a6ca5be0f3cbe5d4af8
mohammad alsboul <mohammad.alspoole@gmail.com>
2019-12-21 14:47:57 +0200

    lab5

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require ('express');
 const server = express ();
 const PORT = process.env.PORT || 5500;
+server.use(express.static('public'));
 server.get('/test' , (request , response) => {
   response.send ('work');
 });
